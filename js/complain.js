f3f2888dfc6a0484422ec7beab59ed125a0dca5b
Rui Zhang <rui.zhang@mongodb.com>
2016-10-31 19:36:12 -0700

    deploy Mon Oct 31 19:36:12 PDT 2016

--- js/complain.js
+++ js/complain.js
@@ -5,7 +5,7 @@
 //eslint    quotes=false
 
 /*exported
-    reset show_data_collector show_data_help show_help report_flight
+    reset show_data_collector show_data_help show_help report_flight email_faa open_complaint_link
 */
 var parseLog = function (l) {
     var re = /^([A-Z][a-z]+ [0-9]+, [\d]+:[\d]+:[\d]+) ([ \w]+) \(([A-Z]+):([A-Z]+) ([\w]+) ([\d]+k), ([\d]+ft)\)/g
@@ -94,8 +94,8 @@ var show_data_collector = function () {
         message: $("#data-collector").html(),
         buttons: {
             confirm: {
-                label: 'Add To My Flight List',
-                className: 'btn-success'
+                label: "Add To My Flight List",
+                className: "btn-success"
             },
         },
         callback: function (c) {
@@ -153,19 +153,19 @@ var fr24_find_data = function (s, term) {
         }
         return t[1];
     }
-}
+};
+
 var report_flight = function () {
     var b = {
         "flightinfo": [],
     };
     var a = {};
     var info = $("#enter-flight-data").val();
-    var t = ""
     var now = new Date();
 
     b.flightinfo.push({
         "Key": "time",
-        "Value": now.toLocaleString(),
+        "Value": now.format("mm/dd/yy HH:MM:ss"),
     });
     b.flightinfo.push({
         "Key": "neighborhood",
@@ -176,7 +176,6 @@ var report_flight = function () {
         var v = fr24_find_data(info, k);
 
         if ((k.indexOf("_m") > -1 || k.indexOf("_f") > -1) && typeof v !== "undefined") {
-            console.log("found value for " + k + " | " + k.slice(0, -2));
             b.flightinfo.push({
                 "Key": k.slice(0, -2),
                 "Value": v
@@ -201,7 +200,7 @@ var report_flight = function () {
     var theCompiledHtml = theTemplate(b);
 
     $("#flight-details").html(theCompiledHtml);
-}
+};
 
 var show_help = function () {
     var bx = bootbox.confirm({
@@ -228,13 +227,11 @@ var show_help = function () {
         callback: function (r) {
             if (r == false) {
                 localStorage.help_showed = false;
-                console.log("show help 1");
                 show_help();
                 bootbox.alert("You must enter name, address, and neighborhood<br>All information is stored locally");
             } else {
                 if ($("#name-input").val() == "" || $("#address-input").val() == "" || $("#neighborhood-input").val() == "") {
                     localStorage.help_showed = false;
-                    console.log("show help 2");
                     show_help();
                     bootbox.alert("You must enter name, address, and neighborhood<br>All information is stored locally");
                 } else {
@@ -288,7 +285,7 @@ var email_faa = function (email, index, flight, airport) {
         $("#email_link_" + index).addClass("btn-default");
     }
     window.open(url, target);
-}
+};
 
 var warnTemplateUpdate = false;
 var defaultTemplate;
@@ -298,7 +295,6 @@ var load = function () {
         defaultTemplate = $("#template").val();
     }
     if (localStorage.help_showed === "false" || typeof localStorage.Address == "undefined" || typeof localStorage.Name == "undefined") {
-        console.log("show help 3");
         show_help();
         return;
     } else {
@@ -309,7 +305,6 @@ var load = function () {
         save();
         localStorage.help_showed = false;
         $("#acked").html(" | Acknowledged Term: <b>false</b>");
-        console.log("show help 4");
         show_help();
         return;
     }
@@ -363,3 +358,118 @@ $(function () {
         }
     });
 });
+
+
+// dateformat
+
+var dateFormat = function () {
+    var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+        timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+        timezoneClip = /[^-+\dA-Z]/g,
+        pad = function (val, len) {
+            val = String(val);
+            len = len || 2;
+            while (val.length < len) val = "0" + val;
+            return val;
+        };
+
+    // Regexes and supporting functions are cached through closure
+    return function (date, mask, utc) {
+        var dF = dateFormat;
+
+        // You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+        if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+            mask = date;
+            date = undefined;
+        }
+
+        // Passing date through Date applies Date.parse, if necessary
+        date = date ? new Date(date) : new Date;
+        if (isNaN(date)) throw SyntaxError("invalid date");
+
+        mask = String(dF.masks[mask] || mask || dF.masks["default"]);
+
+        // Allow setting the utc argument via the mask
+        if (mask.slice(0, 4) == "UTC:") {
+            mask = mask.slice(4);
+            utc = true;
+        }
+
+        var _ = utc ? "getUTC" : "get",
+            d = date[_ + "Date"](),
+            D = date[_ + "Day"](),
+            m = date[_ + "Month"](),
+            y = date[_ + "FullYear"](),
+            H = date[_ + "Hours"](),
+            M = date[_ + "Minutes"](),
+            s = date[_ + "Seconds"](),
+            L = date[_ + "Milliseconds"](),
+            o = utc ? 0 : date.getTimezoneOffset(),
+            flags = {
+                d: d,
+                dd: pad(d),
+                ddd: dF.i18n.dayNames[D],
+                dddd: dF.i18n.dayNames[D + 7],
+                m: m + 1,
+                mm: pad(m + 1),
+                mmm: dF.i18n.monthNames[m],
+                mmmm: dF.i18n.monthNames[m + 12],
+                yy: String(y).slice(2),
+                yyyy: y,
+                h: H % 12 || 12,
+                hh: pad(H % 12 || 12),
+                H: H,
+                HH: pad(H),
+                M: M,
+                MM: pad(M),
+                s: s,
+                ss: pad(s),
+                l: pad(L, 3),
+                L: pad(L > 99 ? Math.round(L / 10) : L),
+                t: H < 12 ? "a" : "p",
+                tt: H < 12 ? "am" : "pm",
+                T: H < 12 ? "A" : "P",
+                TT: H < 12 ? "AM" : "PM",
+                Z: utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+                o: (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+                S: ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+            };
+
+        return mask.replace(token, function ($0) {
+            return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+        });
+    };
+}();
+
+// Some common format strings
+dateFormat.masks = {
+    "default": "ddd mmm dd yyyy HH:MM:ss",
+    shortDate: "m/d/yy",
+    mediumDate: "mmm d, yyyy",
+    longDate: "mmmm d, yyyy",
+    fullDate: "dddd, mmmm d, yyyy",
+    shortTime: "h:MM TT",
+    mediumTime: "h:MM:ss TT",
+    longTime: "h:MM:ss TT Z",
+    isoDate: "yyyy-mm-dd",
+    isoTime: "HH:MM:ss",
+    isoDateTime: "yyyy-mm-dd'T'HH:MM:ss",
+    isoUtcDateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'"
+};
+
+// Internationalization strings
+dateFormat.i18n = {
+    dayNames: [
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ],
+    monthNames: [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ]
+};
+
+// For convenience...
+Date.prototype.format = function (mask, utc) {
+    return dateFormat(this, mask, utc);
+};
